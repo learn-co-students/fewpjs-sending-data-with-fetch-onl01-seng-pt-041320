87124e2be66c4588a3f2ef4c918a3cff8b2a0c17
OrkunSA <c.orkun.s@gmail.com>
2020-10-28 21:15:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: "Steve",
+        email: "steve@steve.com" 
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    const result = fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object){
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    });
+    return result 
+};
