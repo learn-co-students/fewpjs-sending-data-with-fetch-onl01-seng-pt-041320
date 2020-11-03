c51ce021575ca34e9d321e153d5d292265d3505a
dnodolf1212 <dnodolf@gmail.com>
2020-11-03 11:38:27 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-// Add your code here
+//fetch("http://localhost:3000/dogs", { 
+//  method: 'post', 
+//  headers: {
+//    "Content-Type": "application/json",
+//    "Accept": "application/json", 
+//  },
+//  body: JSON.stringify({
+//   dogName: "Byron",
+//   dogBreed: "Poodle"
+//  })
+//});
+//------------------------------------------------
+//let formData = {
+//  dogName: "Byron",
+//  dogBreed: "Poodle"
+//};
+ 
+//let configObj = {
+//  method: 'post'
+//  headers: {
+//    "Content-Type": "application/json",
+//    "Accept": "application/json"
+//  },
+//  body: JSON.stringify(formData)
+//};
+ 
+//fetch("http://localhost:3000/dogs", configObj)
+//  .then(function(response) {
+//    return response.json();
+//  })
+//  .then(function(object) {
+//    console.log(object);
+//  })
+//  .catch(function(error) {
+//    alert("Bad things! Ragnarők!");
+//    console.log(error.message);
+//  });
+
+function submitData(name, email) {
+  
+  let formData = {
+    name: name,
+    email: email
+  };
+  
+  let configObj = {
+    method: 'post',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(object){
+      let h2 = document.createElement('h2');
+        h2.innerHTML = object.id;
+            document.body.appendChild(h2);
+      console.log(object);
+    })
+    .catch(function(error) {
+      let h3 = document.createElement('h3');
+      h3.innerHTML = error.message;
+      document.body.appendChild(h3);
+      console.log(error.message);
+  });
+}
