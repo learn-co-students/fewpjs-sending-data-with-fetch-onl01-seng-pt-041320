448457b8fa8ec4b4eded8e7255eab5c3f5a05d91
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-11-07 20:32:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+    let data = {
+        name: `${name}`,
+        email: `${email}`
+    }; 
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+    let p = document.createElement("p")
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            p.innerHTML = object.id
+            document.body.appendChild(p)
+        })
+        .catch(function(error) {
+            p.innerHTML = error.message
+            document.body.appendChild(p)
+        })
+
+};
